9e4f9bdeb6bc07caa843599c0390343a737b5869
Danilson Reis <danilson.reis@devgo.org>
2023-06-04 13:15:16 -0100

    Added date filter

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,14 @@
 import NavBar from "../../components/NavBar";
 import Categories from "../../components/Categories";
 import Cards from "../../components/Cards";
+import SearchBar from "../../components/SearchBar";
 
 export default function Home() {
   return (
     <>
       <NavBar />
       <Categories />
+      <SearchBar />
       <Cards />
     </>
   );
