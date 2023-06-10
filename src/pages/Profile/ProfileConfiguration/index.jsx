39869587e3c182cae6d14defd8a69ccf81560f7f
rubenmartins21 <rubenmartins463@gmail.com>
2023-06-10 09:00:53 -0100

    help and support area added

--- src/pages/Profile/ProfileConfiguration/index.jsx
+++ src/pages/Profile/ProfileConfiguration/index.jsx
@@ -17,9 +17,15 @@ import {
   TextField,
   InputAdornment,
   ListItem,
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
 } from "@mui/material";
 
 import DeleteIcon from "@mui/icons-material/Delete";
+
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
 import "./ProfileConfiguration.css";
 
 const ProfileConfiguration = () => {
@@ -66,25 +72,170 @@ const ProfileConfiguration = () => {
                     selected={selectedIndex === 1}
                     onClick={(event) => handleListItemClick(event, 1)}
                   >
-                    <ListItemText primary="Remover conta" />
+                    <ListItemText primary="Ajuda e suporte" />
                   </ListItemButton>
                   <ListItemButton
                     selected={selectedIndex === 2}
                     onClick={(event) => handleListItemClick(event, 2)}
                   >
-                    <ListItemText primary="Ajuda e suporte" />
+                    <ListItemText primary="Reportar erro" />
                   </ListItemButton>
                   <ListItemButton
                     selected={selectedIndex === 3}
                     onClick={(event) => handleListItemClick(event, 3)}
                   >
-                    <ListItemText primary="Reportar erro" />
+                    <ListItemText primary="Remover conta" />
                   </ListItemButton>
                 </List>
               </Paper>
             </Grid>
             <Grid item xs={6} md={8}>
               {selectedIndex === 1 && (
+                <Paper
+                  elevation={3}
+                  className=""
+                  sx={{
+                    padding: "1rem",
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                  }}
+                >
+                  <Typography
+                    variant="h5"
+                    sx={{
+                      marginTop: "1rem",
+                      marginBottom: "1rem",
+                      fontWeight: "bold",
+                    }}
+                  >
+                    Ajuda e suporte
+                  </Typography>
+                  <div className="help-topics-area">
+                    <div>
+                      <Accordion>
+                        <AccordionSummary
+                          expandIcon={<ExpandMoreIcon />}
+                          aria-controls="panel1a-content"
+                          id="panel1a-header"
+                        >
+                          <Typography>
+                            Como adicionar uma publicação?{" "}
+                          </Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                          <Typography>
+                            Deve clicar no ícone com a sua foto de perfil que
+                            aparece no canto superior direito do site, após isto
+                            deve clicar em "Perfil", se ainda não tiver nenhuma
+                            publicação vai aparecer uma área escrita "Adicionar
+                            Evento", no caso de ser um evento que deseja
+                            adicionar, para finalizar deve preencher o
+                            formulário que aparecera com as informações do
+                            evento.
+                          </Typography>
+                        </AccordionDetails>
+                      </Accordion>
+                      <Accordion>
+                        <AccordionSummary
+                          expandIcon={<ExpandMoreIcon />}
+                          aria-controls="panel2a-content"
+                          id="panel2a-header"
+                        >
+                          <Typography>
+                            Como mudar as informações do meu perfil?
+                          </Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                          <Typography>
+                            Deve clicar no ícone com a sua foto de perfil que
+                            aparece no canto superior direito do site, após isto
+                            deve clicar em "Perfil", clicar no botão "editar
+                            perfil" que aparece no lado esquerdo da página, isto
+                            encaminhara-lhe para uma secção onde poderá editar
+                            as informações relacionadas com o seu perfil.
+                          </Typography>
+                        </AccordionDetails>
+                      </Accordion>
+                      <Accordion>
+                        <AccordionSummary
+                          expandIcon={<ExpandMoreIcon />}
+                          aria-controls="panel2a-content"
+                          id="panel2a-header"
+                        >
+                          <Typography>
+                            Como mudar a minha foto de perfil e capa?
+                          </Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                          <Typography>
+                            Deve clicar no ícone com a sua foto de perfil que
+                            aparece no canto superior direito do site, após isto
+                            deve clicar em "Perfil", no seu perfil deve clicar
+                            em cima da sua foto de perfil para selecionar uma
+                            nova foto, o mesmo processo deve ser feito para
+                            adicionar uma nova foto de capa.
+                          </Typography>
+                        </AccordionDetails>
+                      </Accordion>
+                      <Accordion>
+                        <AccordionSummary
+                          expandIcon={<ExpandMoreIcon />}
+                          aria-controls="panel2a-content"
+                          id="panel2a-header"
+                        >
+                          <Typography>Como reportar um erro?</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                          <Typography>
+                            Deve clicar no ícone com a sua foto de perfil que
+                            aparece no canto superior direito do site, clique no
+                            ícone de "Configurações" que aparece no lado
+                            esquerdo da página debaixo do botão "Editar perfil",
+                            clique no item "Reportar erro" que aparece no menu
+                            do lado esquerdo da página.
+                          </Typography>
+                        </AccordionDetails>
+                      </Accordion>
+                      <Accordion>
+                        <AccordionSummary
+                          expandIcon={<ExpandMoreIcon />}
+                          aria-controls="panel2a-content"
+                          id="panel2a-header"
+                        >
+                          <Typography>Como remover a minha conta?</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                          <Typography>
+                            Deve clicar no ícone com a sua foto de perfil que
+                            aparece no canto superior direito do site, clique no
+                            ícone de "Configurações" que aparece no lado
+                            esquerdo da página debaixo do botão "Editar perfil",
+                            clique no item "Remover conta" que aparece no menu
+                            do lado esquerdo da página.
+                          </Typography>
+                        </AccordionDetails>
+                      </Accordion>
+                    </div>
+                  </div>
+                </Paper>
+              )}
+              {selectedIndex === 2 && (
+                <Paper
+                  elevation={3}
+                  className=""
+                  sx={{
+                    padding: "1rem",
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                  }}
+                >
+                  Reportar erro
+                </Paper>
+              )}
+
+              {selectedIndex === 3 && (
                 <Paper
                   elevation={3}
                   className=""
@@ -139,34 +290,6 @@ const ProfileConfiguration = () => {
                       </InputAdornment>
                     </div>
                   </div>
-                </Paper>
-              )}
-              {selectedIndex === 2 && (
-                <Paper
-                  elevation={3}
-                  className=""
-                  sx={{
-                    padding: "1rem",
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                  }}
-                >
-                  Ajuda e suporte
-                </Paper>
-              )}
-              {selectedIndex === 3 && (
-                <Paper
-                  elevation={3}
-                  className=""
-                  sx={{
-                    padding: "1rem",
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                  }}
-                >
-                  Reportar erro
                 </Paper>
               )}
             </Grid>
