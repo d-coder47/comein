260ecacf079799c284a2ecd2e557fffaef75a43b
RubenMartins97 <rubendevgo@gmail.com>
2023-06-29 19:47:14 -0100

    params userID and name added to user profile route

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,26 +1,26 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import UserRegistration from "./pages/UserRegistration";
-import Login from "./pages/Login";
-import Home from "./pages/Home";
-import Profile from "./pages/Profile";
-import EditProfile from "./pages/Profile/EditProfile";
-import ProfileConfiguration from "./pages/Profile/ProfileConfiguration";
-import ResetPassword from "./pages/ResetPassword";
+import UserRegistration from './pages/UserRegistration';
+import Login from './pages/Login';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import EditProfile from './pages/Profile/EditProfile';
+import ProfileConfiguration from './pages/Profile/ProfileConfiguration';
+import ResetPassword from './pages/ResetPassword';
 
 const AppRoutes = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/user-registration" element={<UserRegistration />} />
-        <Route path="/user-reset-password" element={<ResetPassword />} />
-        <Route path="/user-login" element={<Login />} />
-        <Route path="/user-profile" element={<Profile />} />
-        <Route path="/edit-profile" element={<EditProfile />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/user-registration' element={<UserRegistration />} />
+        <Route path='/user-reset-password' element={<ResetPassword />} />
+        <Route path='/user-login' element={<Login />} />
+        <Route path='/user-profile/:userid/:nome' element={<Profile />} />
+        <Route path='/edit-profile' element={<EditProfile />} />
         <Route
-          path="/user-profile-configuration"
+          path='/user-profile-configuration'
           element={<ProfileConfiguration />}
         />
       </Routes>
