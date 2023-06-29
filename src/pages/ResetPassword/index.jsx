439d5c3d67e34f16718fd8189fcb14e0a51eb612
RubenMartins97 <rubendevgo@gmail.com>
2023-06-29 19:19:55 -0100

    reset password form improved

--- src/pages/ResetPassword/index.jsx
+++ src/pages/ResetPassword/index.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 import {
   Typography,
   Grid,
@@ -6,28 +6,22 @@ import {
   Box,
   IconButton,
   Button,
-  List,
   InputAdornment,
-  ListItemButton,
-  ListItemText,
   TextField,
   FormLabel,
   Collapse,
-  MenuItem,
   Alert,
   AlertTitle,
-  Autocomplete,
-  useMediaQuery,
-} from "@mui/material";
+} from '@mui/material';
 
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 
-import { useTranslation } from "react-i18next";
-import CloseIcon from "@mui/icons-material/Close";
-import Visibility from "@mui/icons-material/Visibility";
-import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import { useTranslation } from 'react-i18next';
+import CloseIcon from '@mui/icons-material/Close';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
-import useRegisterUser from "../../hooks/useRegisterUser";
+import useRegisterUser from '../../hooks/useRegisterUser';
 
 export default function ResetPassword() {
   const { t } = useTranslation();
@@ -44,19 +38,19 @@ export default function ResetPassword() {
   const [openChangePassSucc, setOpenChangePassSucc] = useState(false);
 
   const queryParameters = new URLSearchParams(window.location.search);
-  const key = queryParameters.get("key");
-  const email = queryParameters.get("email");
+  const key = queryParameters.get('key');
+  const email = queryParameters.get('email');
 
   const { verifyForgotPassEmailLink, changeForgotPass } = useRegisterUser();
 
   const [formData, setFormData] = useState({
-    newPassword: "",
-    confPassword: "",
+    newPassword: '',
+    confPassword: '',
   });
 
   const [formErrors, setFormErrors] = useState({
-    newPassword: "",
-    confPassword: "",
+    newPassword: '',
+    confPassword: '',
   });
 
   const handleMouseDownPassword = (event) => {
@@ -75,9 +69,9 @@ export default function ResetPassword() {
   useEffect(() => {
     async function fetchData() {
       const verfify_res = await verifyForgotPassEmailLink(email, key);
-      // if (!verfify_res.dados) {
-      //   navigate("/");
-      // }
+      if (!verfify_res.dados) {
+        navigate('/');
+      }
     }
     fetchData();
   }, []);
@@ -87,16 +81,16 @@ export default function ResetPassword() {
 
     let errors = {};
 
-    if (formData.newPassword.trim() === "") {
-      errors.newPassword = t("editProfilePage.palavraPasseNovaObrigatorio");
+    if (formData.newPassword.trim() === '') {
+      errors.newPassword = t('editProfilePage.palavraPasseNovaObrigatorio');
       setShowNewPasswordError(true);
     } else {
       setShowNewPasswordError(false);
     }
 
-    if (formData.confPassword.trim() === "") {
+    if (formData.confPassword.trim() === '') {
       errors.confPassword = t(
-        "editProfilePage.confPalavraPasseNovaObrigatorio"
+        'editProfilePage.confPalavraPasseNovaObrigatorio'
       );
       setShowConfPasswordError(true);
     } else {
@@ -104,14 +98,14 @@ export default function ResetPassword() {
     }
 
     if (formData.newPassword.trim() !== formData.confPassword.trim()) {
-      errors.confPassword = t("editProfilePage.palavraPasseNCoincidem");
+      errors.confPassword = t('editProfilePage.palavraPasseNCoincidem');
       setShowConfPasswordError(true);
     } else {
       setShowConfPasswordError(false);
     }
 
-    if (formData.confPassword.trim() === "") {
-      errors.confPassword = t("editProfilePage.confPassObrigatorio");
+    if (formData.confPassword.trim() === '') {
+      errors.confPassword = t('editProfilePage.confPassObrigatorio');
       setShowConfPasswordError(true);
     } else {
       setShowConfPasswordError(false);
@@ -131,170 +125,187 @@ export default function ResetPassword() {
   return (
     <Paper
       sx={{
-        width: "100vw",
-        height: "100vh",
-        background: "#f3f3f3",
+        width: '100vw',
+        height: '100vh',
+        background: '#f3f3f3',
 
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
       }}
     >
-      <Button href="/">{t("forgotPassword.voltarHome")}</Button>
+      <Button href='/'>{t('forgotPassword.voltarHome')}</Button>
 
       <Box
-        component="form"
-        className="reset_pass_form"
+        component='form'
+        className='reset_pass_form'
         sx={{
-          "& .MuiTextField-root": {
+          '& .MuiTextField-root': {
             m: 1,
-            width: "40ch",
+            width: '40ch',
           },
-          display: "flex",
-          flexDirection: "column",
-          width: "500px",
-          height: "400px",
-          alignItems: "center",
-          justifyContent: "center",
-          background: "#fff",
-          marginTop: "10px",
+          display: 'flex',
+          flexDirection: 'column',
+          width: '500px',
+          height: '400px',
+          alignItems: 'center',
+          justifyContent: 'center',
+          background: '#fff',
+          marginTop: '10px',
         }}
         noValidate
-        autoComplete="off"
+        autoComplete='off'
         onSubmit={handleChangePassSubmit}
       >
         <Typography
-          id="modal-modal-title"
-          variant="h6"
-          component="h2"
-          fontWeight="bold"
-          fontSize="18px"
-          textAlign="center"
+          id='modal-modal-title'
+          variant='h6'
+          component='h2'
+          fontWeight='bold'
+          fontSize='18px'
+          textAlign='center'
           sx={{
-            marginBottom: "10px",
+            marginBottom: '10px',
           }}
         >
-          {t("forgotPassword.resetPassword")}
+          {t('forgotPassword.resetPassword')}
         </Typography>
         <Grid
           container
+          justifyContent='center'
           spacing={2}
           sx={{
-            display: "flex",
-            marginLeft: "0px",
-            width: "100%",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
+            marginLeft: '0px',
+            width: '100%',
           }}
         >
-          <Grid item xs={6} textAlign="left">
-            <FormLabel
-              id="new_reset_password_label"
-              sx={{
-                marginLeft: "8px",
-              }}
-            >
-              {t("editProfilePage.novoPass")}
-            </FormLabel>
-            <TextField
-              id="new_password"
-              name="newPassword"
-              variant="outlined"
-              type={showNewPassword ? "text" : "password"}
-              value={formData.newPassword}
-              onChange={handleInputChange}
-              error={showNewPasswordError}
-              helperText={formErrors.newPassword}
-              InputLabelProps={{
-                shrink: true,
-              }}
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="toggle password visibility"
-                      onClick={handleClickShowNewPassword}
-                      onMouseDown={handleMouseDownPassword}
-                      edge="end"
-                    >
-                      {showNewPassword ? <VisibilityOff /> : <Visibility />}
-                    </IconButton>
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </Grid>
-          <Grid item xs={6} textAlign="left">
-            <FormLabel
-              id="conf_password_label"
-              sx={{
-                marginLeft: "8px",
-              }}
-            >
-              {t("editProfilePage.confirmPass")}
-            </FormLabel>
-            <TextField
-              id="conf_password"
-              name="confPassword"
-              variant="outlined"
-              type={showConfPassword ? "text" : "password"}
-              value={formData.confPassword}
-              onChange={handleInputChange}
-              error={showConfPasswordError}
-              helperText={formErrors.confPassword}
-              InputLabelProps={{
-                shrink: true,
-              }}
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="toggle password visibility"
-                      onClick={handleClickShowConfPassword}
-                      onMouseDown={handleMouseDownPassword}
-                      edge="end"
-                    >
-                      {showConfPassword ? <VisibilityOff /> : <Visibility />}
-                    </IconButton>
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </Grid>
-        </Grid>
-        <Grid container justifyContent="center">
-          <Button
-            type="submit"
-            variant="contained"
-            className="change_pass_button"
-            color="primary"
+          <Box
             sx={{
-              m: 3,
-              color: "#ffffff",
-              width: "20ch",
-              borderRadius: "16px",
-              textTransform: "none",
+              marginTop: '20px',
             }}
           >
-            {t("editProfilePage.guardar")}
+            <Grid
+              display='flex'
+              flexDirection='column'
+              justifyContent='center'
+              item
+              xs={12}
+              textAlign='left'
+            >
+              <FormLabel
+                id='new_reset_password_label'
+                sx={{
+                  marginLeft: '8px',
+                }}
+              >
+                {t('editProfilePage.novoPass')}
+              </FormLabel>
+              <TextField
+                id='new_password'
+                name='newPassword'
+                variant='outlined'
+                type={showNewPassword ? 'text' : 'password'}
+                value={formData.newPassword}
+                onChange={handleInputChange}
+                error={showNewPasswordError}
+                helperText={formErrors.newPassword}
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position='end'>
+                      <IconButton
+                        aria-label='toggle password visibility'
+                        onClick={handleClickShowNewPassword}
+                        onMouseDown={handleMouseDownPassword}
+                        edge='end'
+                      >
+                        {showNewPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </Grid>
+            <Grid
+              display='flex'
+              flexDirection='column'
+              justifyContent='center'
+              item
+              xs={12}
+              textAlign='left'
+            >
+              <FormLabel
+                id='conf_password_label'
+                sx={{
+                  marginLeft: '8px',
+                }}
+              >
+                {t('editProfilePage.confirmPass')}
+              </FormLabel>
+              <TextField
+                id='conf_password'
+                name='confPassword'
+                variant='outlined'
+                type={showConfPassword ? 'text' : 'password'}
+                value={formData.confPassword}
+                onChange={handleInputChange}
+                error={showConfPasswordError}
+                helperText={formErrors.confPassword}
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position='end'>
+                      <IconButton
+                        aria-label='toggle password visibility'
+                        onClick={handleClickShowConfPassword}
+                        onMouseDown={handleMouseDownPassword}
+                        edge='end'
+                      >
+                        {showConfPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </Grid>
+          </Box>
+        </Grid>
+        <Grid container justifyContent='center'>
+          <Button
+            type='submit'
+            variant='contained'
+            className='change_pass_button'
+            color='primary'
+            sx={{
+              m: 3,
+              color: '#ffffff',
+              width: '20ch',
+              borderRadius: '16px',
+              textTransform: 'none',
+            }}
+          >
+            {t('editProfilePage.guardar')}
           </Button>
         </Grid>
         <Grid>
           <Collapse in={openChangePassSucc}>
             <Alert
-              severity="success"
+              severity='success'
               action={
                 <IconButton
-                  aria-label="close"
-                  color="inherit"
-                  size="small"
+                  aria-label='close'
+                  color='inherit'
+                  size='small'
                   onClick={() => {
                     setOpenChangePassSucc(false);
                   }}
                 >
-                  <CloseIcon fontSize="inherit" />
+                  <CloseIcon fontSize='inherit' />
                 </IconButton>
               }
               sx={{ mb: 2 }}
@@ -302,7 +313,7 @@ export default function ResetPassword() {
               <AlertTitle>
                 <strong>
                   <strong>
-                    {t("editProfilePage.palavraPassAtualizadoSucesso")}
+                    {t('editProfilePage.palavraPassAtualizadoSucesso')}
                   </strong>
                 </strong>
               </AlertTitle>
@@ -312,24 +323,24 @@ export default function ResetPassword() {
         <Grid>
           <Collapse in={openChangePassError}>
             <Alert
-              severity="error"
+              severity='error'
               action={
                 <IconButton
-                  aria-label="close"
-                  color="inherit"
-                  size="small"
+                  aria-label='close'
+                  color='inherit'
+                  size='small'
                   onClick={() => {
                     setOpenChangePassError(false);
                   }}
                 >
-                  <CloseIcon fontSize="inherit" />
+                  <CloseIcon fontSize='inherit' />
                 </IconButton>
               }
               sx={{ mb: 2 }}
             >
               <AlertTitle>
                 <strong>
-                  {t("editProfilePage.erroAtualizarPalavraPasse")}
+                  {t('editProfilePage.erroAtualizarPalavraPasse')}
                 </strong>
               </AlertTitle>
             </Alert>
