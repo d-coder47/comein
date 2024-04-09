dd7699d00f043c796423a3d1eef19a0a76ce1a51
RubenMartins97 <rubendevgo@gmail.com>
2024-04-09 15:59:20 -0100

    feat: password is needed when removing account

--- src/components/CustomizedAutoComplete.jsx
+++ src/components/CustomizedAutoComplete.jsx
@@ -3,7 +3,7 @@ import CheckIcon from "@mui/icons-material/Check";
 import CloseIcon from "@mui/icons-material/Close";
 import { styled } from "@mui/material/styles";
 import { autocompleteClasses } from "@mui/material/Autocomplete";
-import { useAutocomplete } from "@mui/base/useAutocomplete";
+import { useAutocomplete } from "@mui/base";
 
 const Root = styled("div")(
   ({ theme }) => `
